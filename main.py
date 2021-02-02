8ee740cf76e303bab859e59bf1abc917769ae322
zainkhan <zain.kha.al@gmail.com>
2021-02-02 16:42:37 -0500

    Initial VAE and CAN model weights and prediction code

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ from CAN import CAN
 
 def main():
     # Set parameters
-    vae_epoch = 10
+    vae_epoch = 10000
     vae_batch_size = 128
     latent_dim = 10
     beta_eeg = 1.0
     test_split = 0.2
-
-    can_epoch = 10
+    can_epoch = 10000
     can_batch_size = 16
+    train = False
 
     # Read data sets
     erp, pupil = read_data_sets()
@@ -27,29 +27,43 @@ def main():
         erp, pupil, test_size=test_split
     )
 
-    # Train VAE
-    vae = VAE(beta=beta_eeg, latent_dim=latent_dim)
-    vae.compile(optimizer=keras.optimizers.Adam())
-    vae.fit(erp_train, epochs=vae_epoch, batch_size=vae_batch_size)
+    if train:
+        # Train VAE
+        vae = VAE(beta=beta_eeg, latent_dim=latent_dim)
+        vae.compile(optimizer=keras.optimizers.Adam())
+        vae.fit(erp_train, epochs=vae_epoch, batch_size=vae_batch_size)
+
+        # Train CAN
+        can = CAN(vae=vae, vae_data=erp_train, latent_dim=latent_dim)
+        can.compile(optimizer=keras.optimizers.Adam())
+        can.fit(pupil_train, epochs=can_epoch, batch_size=can_batch_size)
+
+        # Save models
+        vae.encoder.save('vae_encoder')
+        vae.decoder.save('vae_decoder') 
+        can.encoder.save('can_encoder')
+        can.decoder.save('can_decoder')
+    else:
+        # Load all encoders/decoders
+        vae = VAE(beta=beta_eeg, latent_dim=latent_dim)
+        vae.encoder = keras.models.load_model('vae_encoder')
+        vae.decoder = keras.models.load_model('vae_decoder')
+
+        can = CAN(vae=vae, vae_data=erp_train, latent_dim=latent_dim)
+        can.encoder = keras.models.load_model('can_encoder')
+        can.decoder = keras.models.load_model('can_decoder')
+
 
     # VAE predictions
     encoded_data = vae.encoder.predict(erp_test)
     decoded_data = vae.decoder.predict(encoded_data)
-    fn = get_filename("data/", "test-erp")
+    fn = get_filename("erp_predictions/", "test-erp")
     # np.save(fn, decoded_data)
 
     encoded_data = vae.encoder.predict(erp)
     decoded_data = vae.decoder.predict(encoded_data)
-    fn = get_filename("data/", "all-erp")
+    fn = get_filename("erp_predictions/", "all-erp")
     # np.save(fn, decoded_data)
-
-    # Train CAN
-    can = CAN(vae=vae, vae_data=erp_train, latent_dim=latent_dim)
-    can.compile(optimizer=keras.optimizers.Adam())
-    can.fit(pupil_train, epochs=can_epoch, batch_size=can_batch_size)
-
-    # Save models
-    pass
 
 
 if __name__ == "__main__":
