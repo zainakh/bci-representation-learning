4df95c9f0704cfa36a77994e81e84be77185c65a
zainkhan <zain.kha.al@gmail.com>
2021-02-11 10:52:41 -0500

    Updated files

--- main.py
+++ main.py
@@ -12,34 +12,42 @@ from CAN import CAN
 
 def main():
     # Set parameters
-    vae_epoch = 10000
+    vae_epoch = 100
     vae_batch_size = 128
     latent_dim = 10
-    beta_eeg = 1.0
-    can_epoch = 10000
+    beta_eeg = 5.0
+    can_epoch = 100
     can_batch_size = 16
     train = True
 
     # Read data sets
     eeg, pupil = utils.read_single_trial_datasets()
     eeg_train, eeg_test, pupil_train, pupil_test = train_test_split(
-        eeg, pupil, test_size=0.2, random_state=42
+        eeg, pupil, test_size=0.2, shuffle=False
     )
 
     if train:
+        '''
         # Train VAE
         vae = VAE(beta=beta_eeg, latent_dim=latent_dim)
         vae.compile(optimizer=keras.optimizers.Adam())
         vae.fit(eeg_train, epochs=vae_epoch, batch_size=vae_batch_size)
+
+        # Save VAE
+        vae.encoder.save("vae_encoder")
+        vae.decoder.save("vae_decoder")
+        '''
+
+        vae = VAE(beta=beta_eeg, latent_dim=latent_dim)
+        vae.encoder = keras.models.load_model("vae_encoder")
+        vae.decoder = keras.models.load_model("vae_decoder")
 
         # Train CAN
         can = CAN(vae=vae, vae_data=eeg_train, latent_dim=latent_dim)
         can.compile(optimizer=keras.optimizers.Adam())
         can.fit(pupil_train, epochs=can_epoch, batch_size=can_batch_size)
 
-        # Save models
-        vae.encoder.save("vae_encoder")
-        vae.decoder.save("vae_decoder")
+        # Save CAN
         can.encoder.save("can_encoder")
         can.decoder.save("can_decoder")
     else:
